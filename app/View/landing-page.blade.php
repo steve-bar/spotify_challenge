e300c8139059328f45491499adc21693ac8fb71a
steve-bar <66009839+steve-bar@users.noreply.github.com>
2024-01-30 15:21:01 +0100

    add login and landing page views

---
+++ app/View/landing-page.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Spotilenge - Your Challenge, Your Playlists</title>
+</head>
+<body>
+    <h1>Spotilenge</h1>
+    <h2>Your Challenge, Your Playlists</h2>
+    <button onclick="window.location.href='login.blade.php';">Login</button>
+</body>
+</html>
+
